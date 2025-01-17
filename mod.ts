64a5b1a61713c31587fabf7b5abcf8cf0ef17ae6
Gnlow <gnlow@protonmail.com>
2025-01-17 18:08:57 +0900

    not working

--- mod.ts
+++ mod.ts
@@ -1,8 +1,15 @@
 import * as fs from "node:fs"
 import * as http from "node:http"
 
-import { clone } from "npm:isomorphic-git@1.25.6"
+import { clone } from "npm:isomorphic-git@1.27.1"
 
 const result = await clone({
     fs,
+    http,
+    dir: "/tutorial",
+    corsProxy: 'https://cors.isomorphic-git.org',
+    url: 'https://github.com/isomorphic-git/isomorphic-git',
+    ref: 'main',
+    singleBranch: true,
+    depth: 10,
 })
